888b31404ad30a3ef8e779d1779bfda638bd8ab6
Enzo <etanyen@students.cs.ubc.ca>
2023-11-19 10:23:57 -0800

    whatever this is

--- frontend/src/Project.jsx
+++ frontend/src/Project.jsx
@@ -5,11 +5,13 @@ function Project(props) {
 	let [liked, setLiked] = useState(false);
 
 	return (
-		<div>
+		<div className="project-container">
 			<h3>{props.title}</h3>
 			{liked && <p>I like {props.title}</p>}
 			<img
 				src={props.imageURL}
+				alt={props.title}
+				className="project-image"
 				style={{ width: "60vw" }}
 				onClick={() => setLiked(!liked)}
 			></img>
